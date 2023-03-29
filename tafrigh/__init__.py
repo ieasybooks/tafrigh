bc75ceb98acdb92d6c988f5f7b956973c04a6687
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2023-03-30 02:04:32 +0300

    Expose Recognizer and TranscriptWriter classes

--- tafrigh/__init__.py
+++ tafrigh/__init__.py
@@ -1,2 +1,4 @@
 from tafrigh.cli import farrigh
+from tafrigh.recognizer import Recognizer
+from tafrigh.transcript_writer import TranscriptWriter
 from tafrigh.types.transcript_type import TranscriptType
